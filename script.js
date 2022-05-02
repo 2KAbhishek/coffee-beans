d12db89e65c71cab16d565dcfd232ab5e1f8e4e5
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-02 23:15:19 +0530

    Add user prompts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-// $=_=>`$=${$};$()`;$()
-
-console.log("Tiny Web");
+window.onload = function () {
+  let userName = prompt("What is your name?");
+  let favBean = prompt("What is your favorite bean?");
+  alert(`Welcome to my site ${userName}\nMy favorite bean is also ${favBean}`);
+};
